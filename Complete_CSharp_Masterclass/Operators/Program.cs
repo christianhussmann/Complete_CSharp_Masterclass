e4f981d0d0b1e89511e7a5e6c52f05fa149829f1
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-08-23 18:39:12 +0200

    More of the course done

---
+++ Complete_CSharp_Masterclass/Operators/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+            
+            //unary operators
+            num3 = -num1;
+            Console.WriteLine("Num3 is {0}", num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? {0}", !isSunny);
+            
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            //post incement
+            Console.WriteLine("num is {0}", num++);
+            //pre increment
+            Console.WriteLine("num is {0}", ++num);
+            
+            
+            //decrement operator
+            num--;
+            Console.WriteLine("num is {0}", num);
+            Console.WriteLine("num is {0}", num--);
+            //pre decrement
+            Console.WriteLine("num is {0}", --num);
+
+
+
+            int result;
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num2 is {0}", result);
+            
+            
+            //relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            
+            Console.WriteLine("result of num1 < num2 is {0}", isLower);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Complete_CSharp_Masterclass/TryAndCatch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number!");
+            string userInput = Console.ReadLine();
+
+            int num1 = 5;
+            int num2 = 0;
+            int result;
+
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cant divide by 0");
+            }
+
+            
+            
+            
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format exception, please enter the correct type next time.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, the number was too long or too short.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("The value was empty(null)");
+            }
+
+            finally
+            {
+                Console.WriteLine("This is called anyways!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
